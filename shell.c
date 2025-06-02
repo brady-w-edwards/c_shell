f28f3649e9901127cdfee8b3c6ab65af04b4a60d
Brady Edwards <bwe731@gmail.com>
2025-06-01 22:05:24 -0500

    started, program compiles

---
+++ shell.c
@@ -0,0 +1,19 @@
+/*
+
+CSHELL a shell written in C
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    // Read input
+    char *input = "Hello";
+
+    // Evaluate
+
+    // Print output
+    printf(input);
+
+    return 0;
+}
